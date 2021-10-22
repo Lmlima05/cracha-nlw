a971b4b0d3b62686717abfc862462cca3ee473bd
Luan Lima <56600173+Lmlima05@users.noreply.github.com>
2021-10-22 09:31:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+const LinksSocialMedia = {
+  github: 'lmlima05',
+  youtube: '',
+  facebook: 'luuan.melo.5',
+  instagran: 'luanm.lima',
+  twitter: 'lmlimati'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
+
+//ARROW FUNCTIONS
